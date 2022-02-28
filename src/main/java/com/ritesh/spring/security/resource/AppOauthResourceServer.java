ac7a7850a6c8b655ff0e63bfc031469178559b8a
Ritesh <riteshrai11786@gmail.com>
2022-02-28 12:30:55 +0530

    first commit

---
+++ src/main/java/com/ritesh/spring/security/resource/AppOauthResourceServer.java
@@ -0,0 +1,11 @@
+package com.ritesh.spring.security.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOauthResourceServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppOauthResourceServer.class, args);
+    }
+}
